79ee13ad04674cdefd8d7aa1e62ca86eff692788
samaayasin <134007979+samaayasin@users.noreply.github.com>
2023-08-07 16:34:51 +0300

    Update HouseExist.java

--- src/main/java/housing/unit/HouseExist.java
+++ src/main/java/housing/unit/HouseExist.java
@@ -8,8 +8,9 @@ public class HouseExist {
 
 	public static boolean cheack(HousingUnit enteredhouse,List<HousingUnit> housingunitlist) {
 		for (HousingUnit house : housingunitlist) {
-		     if(house.equals(enteredhouse))
-		    	return true;
+		     if(house.equals(enteredhouse)) {
+			    	return true;
+		     }
      }
 		
 		return false;
