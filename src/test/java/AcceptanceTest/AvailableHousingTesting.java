9324e2b688c58116e4dd9a4bb8bbee2dc8a378fd
talamyaseen <112306113+talamyaseen@users.noreply.github.com>
2023-08-08 02:00:29 +0300

    Update and rename AvailableHousingTest.java to AvailableHousingTesting.java

--- src/test/java/AcceptanceTest/AvailableHousingTesting.java
+++ src/test/java/AcceptanceTest/AvailableHousingTesting.java
@@ -14,7 +14,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class AvailableHousingTest {
+public class AvailableHousingTesting {
 
 	  private int availability;
 	    private int studentHousingAvailability;
