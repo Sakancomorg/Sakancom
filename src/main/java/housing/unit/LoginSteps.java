6f66d943c64bce4353d0d5d529b7feaa3c130b1a
samaayasin <134007979+samaayasin@users.noreply.github.com>
2023-08-07 08:24:38 +0300

    Update LoginSteps.java

--- src/main/java/housing/unit/LoginSteps.java
+++ src/main/java/housing/unit/LoginSteps.java
@@ -1,62 +1,82 @@
 package housing.unit;
+
 import java.util.Scanner;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class LoginSteps {
-	MyData L=new MyData();
-	public boolean adminflag=false;
-	public boolean tenantflag=false;
-	public boolean ownerflag=false;
-	private Scanner scanner;
-	public LoginSteps (Scanner scanner){
+    private static final Logger logger = Logger.getLogger(LoginSteps.class.getName());
+    MyData L = new MyData();
+    private boolean adminflag = false;
+    private boolean tenantflag = false;
+    private boolean ownerflag = false;
+    private Scanner scanner;
+
+    public LoginSteps(Scanner scanner) {
         this.scanner = scanner;
+    }
+
+    public boolean isAdminflag() {
+		return adminflag;
 	}
-	
-	    public void init() {
-	    	L.initarrayvalues();
-	        System.out.println("=== Login System ===");
-	        boolean isLoggedIn = false;
-	        while (!isLoggedIn) {
-	            System.out.print("Enter username: ");
-	            String username = scanner.nextLine();
-	            System.out.print("Enter password: ");
-	            String password = scanner.nextLine();
-	            isLoggedIn = checkCredentials(username, password);
-	            if (isLoggedIn) {
-	                System.out.println("Login successful!");
-	            } else {
-	                System.out.println("Invalid username or password. Please try again.");
-	            }
-	        }
 
-	    }
+	public void setAdminflag(boolean adminflag) {
+		this.adminflag = adminflag;
+	}
 
-	    public boolean checkCredentials(String username, String password) {
-	    	boolean nameflag=false;
-	    	boolean pasflag=false;
-	    	for(int i=0 ;i<L.userinfo.size();i++) {
-	    		if(L.userinfo.get(i).name.equals(username)) {
-	    			nameflag=true;
-		    		if(L.userinfo.get(i).pa.equals(password)) {
-		    			pasflag=true;
-		    			if(L.userinfo.get(i).role.equals("admin")) {
-		    				adminflag=true;
-		    			}
-		    			else if (L.userinfo.get(i).role.equals("owner")) {
-		    				ownerflag=true;
-		    			}
-		    			else if (L.userinfo.get(i).role.equals("tenant")) {
-		    				tenantflag=true;
-		    			}
-		    			
-		    			break;
-		    		}
+	public boolean isTenantflag() {
+		return tenantflag;
+	}
 
-	    		}
-	    	}
-	    	
-	    	return nameflag&&pasflag;
-	    }
-	
+	public void setTenantflag(boolean tenantflag) {
+		this.tenantflag = tenantflag;
+	}
 
+	public boolean isOwnerflag() {
+		return ownerflag;
+	}
 
+	public void setOwnerflag(boolean ownerflag) {
+		this.ownerflag = ownerflag;
+	}
+
+	public void init() {
+        L.initarrayvalues();
+        logger.info("=== Login System ===");
+        boolean isLoggedIn = false;
+        while (!isLoggedIn) {
+            logger.info("Enter username: ");
+            String username = scanner.nextLine();
+            logger.info("Enter password: ");
+            String password = scanner.nextLine();
+            isLoggedIn = checkCredentials(username, password);
+            if (isLoggedIn) {
+                logger.info("Login successful!");
+            } else {
+                logger.warning("Invalid username or password. Please try again.");
+            }
+        }
+    }
+
+    public boolean checkCredentials(String username, String password) {
+        boolean nameflag = false;
+        boolean pasflag = false;
+        for (int i = 0; i < L.userinfo.size(); i++) {
+            if (L.userinfo.get(i).name.equals(username)) {
+                nameflag = true;
+                if (L.userinfo.get(i).pa.equals(password)) {
+                    pasflag = true;
+                    if (L.userinfo.get(i).role.equals("admin")) {
+                        adminflag = true;
+                    } else if (L.userinfo.get(i).role.equals("owner")) {
+                        ownerflag = true;
+                    } else if (L.userinfo.get(i).role.equals("tenant")) {
+                        tenantflag = true;
+                    }
+                    break;
+                }
+            }
+        }
+        return nameflag && pasflag;
+    }
 }
