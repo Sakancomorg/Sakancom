33d13c3ac0bf04b028d1a3c9b2b896b70f06ecf3
samaayasin <134007979+samaayasin@users.noreply.github.com>
2023-08-07 16:23:29 +0300

    Update HouseExist.java

--- src/main/java/housing/unit/HouseExist.java
+++ src/main/java/housing/unit/HouseExist.java
@@ -3,16 +3,16 @@ package housing.unit;
 import java.util.ArrayList;
 
 public class HouseExist {
-
-    private HouseExist() {
+	private HouseExist() {
     }
 
-    public static boolean check(HousingUnit enteredHouse, ArrayList<HousingUnit> housingUnitList) {
-        for (HousingUnit house : housingUnitList) {
-            if (house.equals(enteredHouse)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	public static boolean cheack(HousingUnit enteredhouse, ArrayList<HousingUnit> HousingUnitList) {
+		for (HousingUnit house : HousingUnitList) {
+		     if(house.equals(enteredhouse))
+		    	return true;
+     }
+		
+		return false;
+	}
+
 }
