d93e81c028b5b2438eead2a25b00b2ff40c6f746
talamyaseen <112306113+talamyaseen@users.noreply.github.com>
2023-08-08 02:00:10 +0300

    Update and rename BookAccommodationTest.java to BookAccommodationTesting.java

--- src/test/java/AcceptanceTest/BookAccommodationTesting.java
+++ src/test/java/AcceptanceTest/BookAccommodationTesting.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class BookAccommodationTest {
+public class BookAccommodationTesting {
 	 private String selectedAccommodation;
 	    private String bookingConfirmation;
 	
