46d50c3ea509762c483d521f3c8f910c9b5fcbf2
samaayasin <134007979+samaayasin@users.noreply.github.com>
2023-08-07 16:15:28 +0300

    Update Apartments.java

--- src/main/java/housing/unit/Apartments.java
+++ src/main/java/housing/unit/Apartments.java
@@ -1,88 +1,87 @@
 package housing.unit;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Apartments {
-	private int numberOfBathrooms;
+    private int numberOfBathrooms;
     private int numberOfBedrooms;
     private boolean hasBalcony;
-    public int getNumberOfBathrooms() {
-		return numberOfBathrooms;
-	}
+    private int floor;
+    private int id;
+    private List<String> tenantNames = new ArrayList<>();
+    private List<String> meansOfCommunication = new ArrayList<>();
 
-	public void setNumberOfBathrooms(int numberOfBathrooms) {
-		this.numberOfBathrooms = numberOfBathrooms;
-	}
-
-	public int getNumberOfBedrooms() {
-		return numberOfBedrooms;
-	}
-
-	public void setNumberOfBedrooms(int numberOfBedrooms) {
-		this.numberOfBedrooms = numberOfBedrooms;
-	}
-
-	public boolean isHasBalcony() {
-		return hasBalcony;
-	}
-
-	public void setHasBalcony(boolean hasBalcony) {
-		this.hasBalcony = hasBalcony;
-	}
-
-	public int getFloor() {
-		return floor;
-	}
-
-	public void setFloor(int floor) {
-		this.floor = floor;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public List<String> getTenantNames() {
-		return tenantNames;
-	}
-
-	public void setTenantNames(List<String> tenantNames) {
-		this.tenantNames = tenantNames;
-	}
-
-	public List<String> getMeansOfCommunication() {
-		return meansOfCommunication;
-	}
-
-	public void setMeansOfCommunication(List<String> meansOfCommunication) {
-		this.meansOfCommunication = meansOfCommunication;
-	}
-	private int floor;
-
-    
-    int id;
-	List<String> tenantNames = new ArrayList<String>();
-	List<String> meansOfCommunication = new ArrayList<String>();
-
-    public Apartments(int floor,int id,List tenantNames,List meansOfCommunication,int numberOfBathrooms, int numberOfBedrooms, boolean hasBalcony) {
+    public Apartments(int floor, int id, List<String> tenantNames, List<String> meansOfCommunication,
+                      int numberOfBathrooms, int numberOfBedrooms, boolean hasBalcony) {
         this.numberOfBathrooms = numberOfBathrooms;
         this.numberOfBedrooms = numberOfBedrooms;
         this.hasBalcony = hasBalcony;
-        this.tenantNames=tenantNames;
-        this.meansOfCommunication=meansOfCommunication;
-        this.id=id;    
-        this.floor=floor;
+        this.tenantNames = tenantNames;
+        this.meansOfCommunication = meansOfCommunication;
+        this.id = id;
+        this.floor = floor;
     }
-    
+
     public Apartments() {
     }
+
+    public int getNumberOfBathrooms() {
+        return numberOfBathrooms;
+    }
+
+    public void setNumberOfBathrooms(int numberOfBathrooms) {
+        this.numberOfBathrooms = numberOfBathrooms;
+    }
+
+    public int getNumberOfBedrooms() {
+        return numberOfBedrooms;
+    }
+
+    public void setNumberOfBedrooms(int numberOfBedrooms) {
+        this.numberOfBedrooms = numberOfBedrooms;
+    }
+
+    public boolean isHasBalcony() {
+        return hasBalcony;
+    }
+
+    public void setHasBalcony(boolean hasBalcony) {
+        this.hasBalcony = hasBalcony;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<String> getTenantNames() {
+        return tenantNames;
+    }
+
+    public void setTenantNames(List<String> tenantNames) {
+        this.tenantNames = tenantNames;
+    }
+
+    public List<String> getMeansOfCommunication() {
+        return meansOfCommunication;
+    }
+
+    public void setMeansOfCommunication(List<String> meansOfCommunication) {
+        this.meansOfCommunication = meansOfCommunication;
+    }
+
     @Override
     public String toString() {
         return "\nID: " + id +
@@ -92,5 +91,4 @@ public class Apartments {
                 ", Bedrooms: " + numberOfBedrooms +
                 ", Balcony: " + hasBalcony;
     }
-
 }
