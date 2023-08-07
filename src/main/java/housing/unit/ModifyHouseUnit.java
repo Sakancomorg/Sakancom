223c026c394f0786a8d1f264ae41f68dec328cd1
samaayasin <134007979+samaayasin@users.noreply.github.com>
2023-08-07 14:19:54 +0300

    Update ModifyHouseUnit.java

--- src/main/java/housing/unit/ModifyHouseUnit.java
+++ src/main/java/housing/unit/ModifyHouseUnit.java
@@ -1,74 +1,63 @@
 package housing.unit;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class ModifyHouseUnit {
-	 public static HousingUnit  housingUnitToModify;
-	 public static HousingUnit  housingUnitToModifyAfter;
-	 static int index;
-	public static void modify()
-	{
-		  System.out.println("Enter the ID of the housing unit to modify: ");
-		 housingUnitToModify = HousingUnit.FindHouse(MyData.getHousingUnitList());
-		 if(housingUnitToModify==null) return;
-		 index=MyData.HousingUnitList.indexOf(housingUnitToModify);
-		 Scanner scanner = new Scanner(System.in);
-	        if (housingUnitToModify == null) {
-	            System.out.println("No housing unit found with the given ID.");
-	            return ;
-	        }
+    private static final Logger logger = Logger.getLogger(ModifyHouseUnit.class.getName());
 
-	        System.out.println("Enter the new location: ");
-	 
-			String newLocation = scanner.next();
-	        housingUnitToModify.setLocation(newLocation);
+    public static HousingUnit housingUnitToModify;
+    public static HousingUnit housingUnitToModifyAfter;
+    static int index;
 
-	        System.out.println("Enter the new photo: ");
-	        String newPhoto = scanner.next();
-	        housingUnitToModify.setPhoto(newPhoto);
+    public static void modify() {
+        logger.info("Enter the ID of the housing unit to modify: ");
+        housingUnitToModify = HousingUnit.FindHouse(MyData.getHousingUnitList());
+        if (housingUnitToModify == null) return;
+        index = MyData.HousingUnitList.indexOf(housingUnitToModify);
+        Scanner scanner = new Scanner(System.in);
+        if (housingUnitToModify == null) {
+            logger.info("No housing unit found with the given ID.");
+            return;
+        }
 
-	        System.out.println("Enter the new rent: ");
-	        double newRent = scanner.nextDouble();
-	        housingUnitToModify.setRent(newRent);
+        logger.info("Enter the new location: ");
+        String newLocation = scanner.next();
+        housingUnitToModify.setLocation(newLocation);
 
+        logger.info("Enter the new photo: ");
+        String newPhoto = scanner.next();
+        housingUnitToModify.setPhoto(newPhoto);
 
-	        System.out.println("Enter the new number of floors: ");
-	        int newNumberOfFloors = scanner.nextInt();
-	        housingUnitToModify.setNumberOfFloors(newNumberOfFloors);
+        logger.info("Enter the new rent: ");
+        double newRent = scanner.nextDouble();
+        housingUnitToModify.setRent(newRent);
 
-	        System.out.println("Enter the new available services: ");
-	        String newAvailableServices = scanner.next();
-	        
-	        System.out.println("Is the residence for Students? (true/false):");
-            boolean Studentflag = scanner.nextBoolean();
-            scanner.nextLine();
-         
-	        housingUnitToModify.setAvailableServices(newAvailableServices);
-	        
+        logger.info("Enter the new number of floors: ");
+        int newNumberOfFloors = scanner.nextInt();
+        housingUnitToModify.setNumberOfFloors(newNumberOfFloors);
 
-	        System.out.println("Housing unit with ID " + housingUnitToModify.getId() + " has been modified.");
-	        
+        logger.info("Enter the new available services: ");
+        String newAvailableServices = scanner.next();
 
-	        
-	        
-	}
-	
-	
-	
-	public static void  modifyAdmin() {
-		 HousingSystem.printData(MyData.getHousingUnitList());
-		 modify();
-		 if(housingUnitToModify==null) {
-			  System.out.println("Not Found");
-			  return;
+        logger.info("Is the residence for Students? (true/false):");
+        boolean Studentflag = scanner.nextBoolean();
+        scanner.nextLine();
 
-		 }
-			 
-		 MyData.HousingUnitList.set(index,housingUnitToModify);
-		 housingUnitToModifyAfter=MyData.HousingUnitList.get(index);}
-	
-	
-	
-	
-	
+        housingUnitToModify.setAvailableServices(newAvailableServices);
+
+        logger.info("Housing unit with ID " + housingUnitToModify.getId() + " has been modified.");
+    }
+
+    public static void modifyAdmin() {
+        HousingSystem.printData(MyData.getHousingUnitList());
+        modify();
+        if (housingUnitToModify == null) {
+            logger.info("Not Found");
+            return;
+        }
+
+        MyData.HousingUnitList.set(index, housingUnitToModify);
+        housingUnitToModifyAfter = MyData.HousingUnitList.get(index);
+    }
 }
