e58bdea915f12e67ec53903c765d4348139e2087
samaayasin <134007979+samaayasin@users.noreply.github.com>
2023-08-07 14:27:01 +0300

    Update TenantSystem.java

--- src/main/java/housing/unit/TenantSystem.java
+++ src/main/java/housing/unit/TenantSystem.java
@@ -1,21 +1,21 @@
 package housing.unit;
 
+import java.util.logging.Logger;
 
 public class TenantSystem {
+    private static final Logger logger = Logger.getLogger(TenantClass.class.getName());
 
 	public static void viewAvailableHousing() {
-		// TODO Auto-generated method stub
 	HousingSystem.printData(MyData.getAvilableHousing());
 	}
 
 
 	public static void book() {
-		// TODO Auto-generated method stub
 		HousingUnit unit =HousingSystem.FindHouse(MyData.getAvilableHousing());
 		int index=MyData.HousingUnitList.indexOf(unit);
 		 MyData.HousingUnitList.get(index).setReservedFlag(true);
 		MyData.addReservationsList(unit);
-		System.out.println("The house is booked");
+		logger.info("The house is booked");
 	}
 
 
