64e179044eff79de916a3d001f888c6fef13b339
samaayasin <134007979+samaayasin@users.noreply.github.com>
2023-08-07 14:12:16 +0300

    Update AdminClass.java

--- src/main/java/housing/unit/AdminClass.java
+++ src/main/java/housing/unit/AdminClass.java
@@ -1,48 +1,46 @@
 package housing.unit;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class AdminClass {
-	static Scanner scanner = new Scanner(System.in);
-	static int choice;
-	public static Boolean re;
-	public void adminmenu() {
+    private static final Logger logger = Logger.getLogger(AdminClass.class.getName());
+    private static Scanner scanner = new Scanner(System.in);
+    private static int choice;
+    public static Boolean re;
+
+    public void adminmenu() {
         do {
-            System.out.println("========= Housing System Menu =========");
-            System.out.println("1. View requests for advertisement");
-            System.out.println("2. View reservations");
-            System.out.println("3. Add a housing unit");
-            System.out.println("4. Modify housing unit");
-            System.out.println("5. Exit");
-            System.out.print("Enter your choice: ");          
-            
+            logger.info("========= Housing System Menu =========");
+            logger.info("1. View requests for advertisement");
+            logger.info("2. View reservations");
+            logger.info("3. Add a housing unit");
+            logger.info("4. Modify housing unit");
+            logger.info("5. Exit");
+            logger.info("Enter your choice: ");
+
             choice = scanner.nextInt();
             scanner.nextLine();
             switch (choice) {
                 case 1:
-                	 HousingSystem.viewAdvertisementRequest();
+                    HousingSystem.viewAdvertisementRequest();
                     break;
                 case 2:
-                	 HousingSystem.viewReservations();
-                	 re=true;
+                    HousingSystem.viewReservations();
+                    re = true;
                     break;
                 case 3:
-                	HousingSystem.addUnit();
+                    HousingSystem.addUnit();
                     break;
                 case 4:
-                         HousingSystem. modifyHousingData();
+                    HousingSystem.modifyHousingData();
                     break;
                 case 5:
-                    System.out.println("Exiting the Housing System. Goodbye!");
+                    logger.info("Exiting the Housing System. Goodbye!");
                     break;
                 default:
-                    System.out.println("Invalid choice. Please try again.");
+                    logger.warning("Invalid choice. Please try again.");
             }
         } while (choice != 5);
-        
-        
-	}
-	
-
-
+    }
 }
