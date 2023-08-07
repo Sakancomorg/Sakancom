ddebc5506901dea35a9d0c22ecbcd823b8059aee
talamyaseen <112306113+talamyaseen@users.noreply.github.com>
2023-08-08 01:59:08 +0300

    Update and rename ReservationTest.java to ReservationTesting.java

--- src/test/java/AcceptanceTest/ReservationTesting.java
+++ src/test/java/AcceptanceTest/ReservationTesting.java
@@ -14,7 +14,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class ReservationTest {
+public class ReservationTesting {
 
    static Boolean ShowFlag;
    AdminClass adminClass;
