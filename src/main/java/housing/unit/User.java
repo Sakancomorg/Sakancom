700536ce575e389ed4ce8c385c3b6070a7fa3825
samaayasin <134007979+samaayasin@users.noreply.github.com>
2023-08-07 13:45:56 +0300

    Update User.java

--- src/main/java/housing/unit/User.java
+++ src/main/java/housing/unit/User.java
@@ -1,11 +1,11 @@
 package housing.unit;
 
 public class User {
-	public String name;
-	public String emaill;
-	public String pa;
+	private String name;
+	private String emaill;
+	private String pa;
 	String role;
-	public int s;
+	private int s;
 
 	User(String role,String name ,String emaill , String pa,int s){
 		this.name=name;
@@ -13,6 +13,46 @@ public class User {
 		this.pa=pa;
 		this.s=s;
 		this.role=role;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmaill() {
+		return emaill;
+	}
+
+	public void setEmaill(String emaill) {
+		this.emaill = emaill;
+	}
+
+	public String getPa() {
+		return pa;
+	}
+
+	public void setPa(String pa) {
+		this.pa = pa;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public int getS() {
+		return s;
+	}
+
+	public void setS(int s) {
+		this.s = s;
 	}
 
 }
