9ed3a1d238378d4d3d92d2c3f68343be6d1ffe50
talamyaseen <112306113+talamyaseen@users.noreply.github.com>
2023-08-08 16:22:42 +0300

    Update AcceptanceTest.java

--- src/test/java/AcceptanceTest/AcceptanceTest.java
+++ src/test/java/AcceptanceTest/AcceptanceTest.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-		features="test_cases/TenantFeatures", 
+		features="test_cases/ownerFeature", 
 
 		plugin="html:target/cucumber/out.html",
 		monochrome=true,
