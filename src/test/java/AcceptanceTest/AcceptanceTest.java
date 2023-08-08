35f7e0d2ffa48a7cd37ae16eb9643d5d541cb7c0
talamyaseen <112306113+talamyaseen@users.noreply.github.com>
2023-08-08 16:42:11 +0300

    Update AcceptanceTest.java

--- src/test/java/AcceptanceTest/AcceptanceTest.java
+++ src/test/java/AcceptanceTest/AcceptanceTest.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-		features="test_cases/AdminFeatures/Reservation.feature", 
+		features="test_cases", 
 
 		plugin="html:target/cucumber/out.html",
 		monochrome=true,
