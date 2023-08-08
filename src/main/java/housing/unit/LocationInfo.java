cfc77f8147f9ec32eb4e2e67e99a67139e6a38c6
samaayasin <134007979+samaayasin@users.noreply.github.com>
2023-08-08 21:04:33 +0300

    Add files via upload

---
+++ src/main/java/housing/unit/LocationInfo.java
@@ -0,0 +1,48 @@
+package housing.unit;
+
+public class LocationInfo {
+	 private String location;
+	    private String photo;
+	    private double rent;
+	    private int id;
+
+	    public LocationInfo(String location, String photo, double rent, int id) {
+	        this.location = location;
+	        this.photo = photo;
+	        this.rent = rent;
+	        this.id = id;
+	    }
+
+		public String getLocation() {
+			return location;
+		}
+
+		public void setLocation(String location) {
+			this.location = location;
+		}
+
+		public String getPhoto() {
+			return photo;
+		}
+
+		public void setPhoto(String photo) {
+			this.photo = photo;
+		}
+
+		public double getRent() {
+			return rent;
+		}
+
+		public void setRent(double rent) {
+			this.rent = rent;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+}
