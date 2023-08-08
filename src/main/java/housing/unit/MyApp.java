156a723c287239afd4c779f44e5ab04d39736321
talamyaseen <112306113+talamyaseen@users.noreply.github.com>
2023-08-08 14:43:11 +0300

    Update MyApp.java

--- src/main/java/housing/unit/MyApp.java
+++ src/main/java/housing/unit/MyApp.java
@@ -1,31 +1,34 @@
-
 package housing.unit;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class MyApp {
-	public static void main(String[] args) { 
-		 String s;
+    private static final Logger LOGGER = Logger.getLogger(MyApp.class.getName());
+
+    public static void main(String[] args) {
+        String s;
         Scanner scanner = new Scanner(System.in);
-		LoginSteps ls;
-		TenantClass t=new TenantClass(scanner);
-		OwnerClass o=new OwnerClass(scanner);
-		AdminClass a=new AdminClass();
-		
-		do {
-		ls= new LoginSteps(scanner);
-		ls.init();
-		if(ls.isAdminflag())
-			a.adminmenu();
-		else if(ls.isOwnerflag())
-			o.ownermenu();
-		else if(ls.isTenantflag()) 
-			t.tenantmenu();
-		
-		System.out.println("Close the App?");
-		s=scanner.nextLine();
-		
-		}while(!s.equalsIgnoreCase("yes"));
+        LoginSteps ls;
+        TenantClass t = new TenantClass(scanner);
+        OwnerClass o = new OwnerClass(scanner);
+        AdminClass a = new AdminClass();
+
+        do {
+            ls = new LoginSteps(scanner);
+            ls.init();
+            if (ls.isAdminflag())
+                a.adminmenu();
+            else if (ls.isOwnerflag())
+                o.ownermenu();
+            else if (ls.isTenantflag())
+                t.tenantmenu();
+
+            LOGGER.info("Close the App?");
+            s = scanner.nextLine();
+
+        } while (!s.equalsIgnoreCase("yes"));
+
         scanner.close();
-	}
+    }
 }
